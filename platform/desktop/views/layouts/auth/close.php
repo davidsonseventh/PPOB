68e4200a33b05ff322a4dc555de9d9af61d60354
Davidson I Rumondor <61097972+davidsonseventh@users.noreply.github.com>
2025-08-30 20:26:34 +0800

    Add files via upload

---
+++ platform/desktop/views/layouts/auth/close.php
@@ -0,0 +1,33 @@
+                            <div class="nk-block nk-auth-footer">
+                                <div class="nk-block-between">
+                                    <ul class="nav nav-sm">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Terms & Condition</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Privacy Policy</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Help</a>
+                                        </li>
+                                    </ul><!-- .nav -->
+                                </div>
+                                <div class="mt-3">
+                                    <p>&copy; 2021 <?= $_CONFIG['title']; ?>. All Rights Reserved.</p>
+                                </div>
+                            </div><!-- .nk-block -->
+                        </div><!-- .nk-split-content -->
+                        <div class="nk-split-content nk-split-stretch bg-abstract"></div>
+                    </div><!-- .nk-split -->
+                </div>
+                <!-- wrap @e -->
+            </div>
+            <!-- content @e -->
+        </div>
+        <!-- main @e -->
+    </div>
+    <!-- app-root @e -->
+    <!-- JavaScript -->
+    <script src="<?= desktop_assets('js/bundle.js?ver=2.2.0'); ?>"></script>
+    <script src="<?= desktop_assets('js/scripts.js?ver=2.2.0'); ?>"></script>
+</html>
